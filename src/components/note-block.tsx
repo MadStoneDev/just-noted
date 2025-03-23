9386900c7cb7532a90effbf7011a8d5a59e7198c
Richard H. <richard@haddads.net.au>
2025-03-24 00:07:46 +1000

    initial mvp

---
+++ src/components/note-block.tsx
@@ -0,0 +1,56 @@
+﻿"use client";
+
+import React, { useEffect, useState } from "react";
+import TextBlock from "@/components/text-block";
+
+export default function NoteBlock() {
+  // States
+  const [noteContent, setNoteContent] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+  const [charCount, setCharCount] = useState(0);
+
+  // Functions
+  const handleChange = (value: string) => {
+    setNoteContent(value);
+
+    const words: string[] = value
+      .replace(/<[^>]*>/g, "") // Remove HTML tags
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word !== "");
+
+    const chars = value.replace(/<[^>]*>/g, "").trim();
+
+    setWordCount(words.length);
+    setCharCount(chars.length);
+  };
+
+  return (
+    <>
+      <div className={`col-span-9 min-w-full`}>
+        <TextBlock
+          value={noteContent}
+          onChange={handleChange}
+          placeholder="Just start typing..."
+        />
+      </div>
+
+      <div
+        className={`col-span-3 flex flex-col justify-center gap-2 text-xl text-neutral-400/70 font-light capitalize`}
+      >
+        <p className={`flex items-center gap-1`}>
+          <span className={`text-mercedes-primary font-normal text-2xl`}>
+            {wordCount}
+          </span>{" "}
+          words
+        </p>
+        <p>
+          <span className={`text-mercedes-primary font-normal text-2xl`}>
+            {charCount}
+          </span>{" "}
+          characters
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/components/text-block.tsx
@@ -0,0 +1,36 @@
+﻿"use client";
+
+import React from "react";
+
+interface Props {
+  value: string;
+  onChange?: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+  [key: string]: any; // Allow for additional props to be passed
+}
+
+export default function TextBlock({
+  value,
+  onChange,
+  placeholder = "Start typing...",
+  className = "",
+  ...props
+}: Props) {
+  // Handle changes to the contenteditable div
+  const handleInput = (event: React.FormEvent<HTMLDivElement>) => {
+    if (onChange) {
+      onChange(event.currentTarget.innerHTML);
+    }
+  };
+
+  return (
+    <div
+      className={`pypx-4 min-h-[200px] max-h-[600px] bg-white rounded-lg shadow-lg shadow-neutral-200/20 outline-2 outline-transparent focus:outline-mercedes-primary font-light overflow-y-auto ${className} transition-all duration-300 ease-in-out`}
+      contentEditable={true}
+      onInput={handleInput}
+      data-placeholder={placeholder}
+      {...props}
+    />
+  );
+}
