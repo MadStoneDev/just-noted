bb3b8aba0bddc6f9b7f2645ab44aab51545651a1
Richard H. <richard@haddads.net.au>
2025-03-26 14:19:24 +1000

    add open graph meta details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import { Outfit, Playfair_Display } from "next/font/google";
 
 import React, { ReactNode } from "react";
-import Link from "next/link";
+
 import GlobalHeader from "@/components/global-header";
 import GlobalFooter from "@/components/global-footer";
 
@@ -20,6 +20,36 @@ const playfair = Playfair_Display({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
+export const metadata = {
+  title: "Just Noted - Distraction-Free Note Taking",
+  description:
+    "Just Noted: Where ideas flow freely. No formatting distractions, just pure writing with offline/online saving and word tools.",
+  openGraph: {
+    title: "Just Noted - Distraction-Free Note Taking",
+    description:
+      "Just Noted: Where ideas flow freely. No formatting distractions, just pure writing with offline/online saving and word tools.",
+    images: [
+      {
+        url: "/JustNoted_OG.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Just Noted - Distraction-Free Note Taking",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+    siteName: "Just Noted",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Just Noted - Distraction-Free Note Taking",
+    description:
+      "Just Noted: Where ideas flow freely. No formatting distractions, just pure writing with offline/online saving and word tools.",
+    images: ["/JustNoted_OG.jpg"],
+    creator: "@justnoted",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,7 +58,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-neutral-200  min-h-dvh flex flex-col ${outfit.variable} ${playfair.variable} antialiased`}
+        className={`bg-neutral-200 min-h-dvh flex flex-col ${outfit.variable} ${playfair.variable} antialiased`}
       >
         <GlobalHeader />
 
