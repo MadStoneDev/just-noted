7017dd2c5557147f79b0968f820a788c13f09da8
Richard H. <richard@haddads.net.au>
2025-05-20 09:22:16 +1000

    remove /me for redirect

--- src/app/get-access/actions.ts
+++ src/app/get-access/actions.ts
@@ -140,7 +140,7 @@ export async function verifyOtp(formData: FormData): Promise<AuthResponse> {
     return {
       error: null,
       success: true,
-      redirectTo: "/me",
+      redirectTo: "/",
     };
   } catch (error: any) {
     console.error("Unexpected error during OTP verification:", error);
